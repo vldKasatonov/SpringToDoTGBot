8f2151f45cc477a1c03d11400e7430d5be2bbe02
Vlad Kasatonov <vld.kasatonov@gmail.com>
2024-08-28 19:22:57 +0300

    init project

---
+++ src/main/java/com/example/springtodotgbot/SpringToDoTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtodotgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToDoTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToDoTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtodotgbot/SpringToDoTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtodotgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToDoTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
